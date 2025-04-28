d3b6c718930663df23bd20af00340373897dec21
SedlarDavid <sedlardavid.cz@gmail.com>
2025-04-28 15:21:24 +0200

    Initial Commit

---
+++ testera/testeraApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeraApp.swift
+//  testera
+//
+//  Created by David Sedlář on 28.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct testeraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ testeraTests/testeraTests.swift
@@ -0,0 +1,16 @@
+//
+//  testeraTests.swift
+//  testeraTests
+//
+//  Created by David Sedlář on 28.04.2025.
+//
+
+import Testing
+
+struct testeraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
